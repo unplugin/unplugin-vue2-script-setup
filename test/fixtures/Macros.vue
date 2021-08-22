df12accfc5187f6a573a11e72d8826dd82c070cc
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-22 20:50:57 +0800

    test: add more tests

---
+++ test/fixtures/Macros.vue
@@ -0,0 +1,11 @@
+<template>
+  <div @click="emit(props.msg)">{{ msg }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  msg: String,
+})
+
+const emit = defineEmits()
+</script>
---
+++ test/fixtures/MacrosType.vue
@@ -0,0 +1,12 @@
+<template>
+  <div @click="emit(props.msg)">{{ msg }}</div>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{ msg: string; value: number }>(),
+  { msg: 'Hello' },
+)
+
+const emit = defineEmits()
+</script>
