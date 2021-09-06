c52503fd6281d1329261848a25e69d165b2ecbbf
Anthony Fu <anthonyfu117@hotmail.com>
2021-09-06 22:42:31 +0800

    docs: update nuxt example, close #36

--- examples/nuxt/nuxt.config.js
+++ examples/nuxt/nuxt.config.js
@@ -2,9 +2,8 @@ export default {
   buildModules: [
     // we disable the type check and left it to `vue-tsc`
     ['@nuxt/typescript-build', { typeCheck: false }],
-    // @vue/composition-api support
+    // @vue/composition-api support,
+    // which ships the `<script setup>` transformer out-of-box
     '@nuxtjs/composition-api/module',
-    // install the transformer
-    'unplugin-vue2-script-setup/nuxt',
   ],
 }
