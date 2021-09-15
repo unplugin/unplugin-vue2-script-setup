3e8186c7d01e96b58281c51458a801711b366919
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-09-15 20:45:32 +0300

    Update playground

--- playground/App.vue
+++ playground/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <hello-world name="Vue 2" @update="onUpdate" />
+
+    <async-component />
   </div>
 </template>
 
 <script setup lang="ts">
+import { defineAsyncComponent } from '@vue/composition-api'
+
 import HelloWorld from './HelloWorld.vue'
+
+const AsyncComponent = defineAsyncComponent(() => import('./Async.vue'))
 
 function onUpdate(e: any) {
   // eslint-disable-next-line no-console
