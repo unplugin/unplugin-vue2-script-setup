b01ab03d2348553efc5dad5021c02906568e994b
Ivan Demchuk <ide@interlogic.com.ua>
2021-09-15 18:36:15 +0300

    Add test

---
+++ test/fixtures/AsyncImport.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'script-setup'
+const ScriptOnly = defineAsyncComponent(() => import('./ScriptOnly.vue'))
+</script>
+
+<template>
+  <ScriptOnly />
+</template>
