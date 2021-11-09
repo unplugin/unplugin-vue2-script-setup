3c9bb597d73291fa00fd995cfeaaee22ed679ae6
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2021-11-10 00:19:03 +0800

    fix: add support for withDefaults call without VariableDeclaration

---
+++ test/fixtures/MacrosType3.vue
@@ -0,0 +1,14 @@
+<template>
+  <div @click="emit('update', msg)">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{ msg: string; value: number | string; data?: { value: boolean } }>(),
+  { msg: 'Hello' },
+)
+
+const emit = defineEmits(['update'])
+</script>
