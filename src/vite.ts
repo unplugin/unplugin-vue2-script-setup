94cb32c2590a504b8f949282e058031089ede753
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2022-02-11 12:50:44 +0800

    fix: esm support

--- src/vite.ts
+++ src/vite.ts
@@ -1,6 +1,3 @@
 import unplugin from '.'
 
 export default unplugin.vite
-module.exports = unplugin.vite
-module.exports.default = unplugin.vite
-Object.defineProperty(module.exports, '__esModule', { value: true })
--- src/webpack.ts
+++ src/webpack.ts
@@ -1,6 +1,3 @@
 import unplugin from '.'
 
 export default unplugin.webpack
-module.exports = unplugin.webpack
-module.exports.default = unplugin.webpack
-Object.defineProperty(module.exports, '__esModule', { value: true })
