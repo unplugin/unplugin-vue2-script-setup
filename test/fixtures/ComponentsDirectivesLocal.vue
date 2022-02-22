cbb887aceb262702dc68cea95395ef786b0dcb82
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2022-02-22 20:33:43 +0800

    fix: get declarations from both `<script setup>` and `<script>`, close #104

---
+++ test/fixtures/ComponentsDirectivesLocal.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import type { ObjectDirective } from '@vue/composition-api'
+// enables v-focus in templates
+const vFocus: ObjectDirective<HTMLInputElement, null> = {
+  inserted: (el, binding, vnode, oldVnode) => {
+    el.focus()
+  },
+}
+</script>
+<script setup lang="ts"></script>
+<template>
+  <input v-focus="null">
+</template>
---
+++ test/fixtures/ComponentsLocal.vue
@@ -0,0 +1,7 @@
+<script lang="tsx">
+const HelloComponent = ({ name }: { name: string }) => <span>Hello, {name}</span>
+</script>
+<script setup lang="tsx"></script>
+<template>
+  <HelloComponent name="Evan You" />
+</template>
