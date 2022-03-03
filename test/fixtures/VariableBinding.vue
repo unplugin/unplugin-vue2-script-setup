a038acbb8788a7eb276e26fa9304be269115e3a3
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2022-03-04 00:19:33 +0800

    chore: update test case

---
+++ test/fixtures/VariableBinding.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+/* eslint-disable no-console */
+import { reactive } from '@vue/composition-api'
+const state = reactive({ value: '' })
+function showEvent(event: MouseEvent) {
+  console.log(event)
+}
+const emit = defineEmits<{
+  (event: 'foo'): void
+}>()
+</script>
+<template>
+  <div
+    @click="
+      state.value = `x: ${$event.x}`
+      showEvent($event)
+      emit('foo')
+    "
+  >
+    2333
+  </div>
+</template>
